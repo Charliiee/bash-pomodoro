af9a29423febeaed4b3bc26da10465c919d72c9a
Carlos <carlos.braga@admcsc.com.br>
2018-06-11 16:44:39 -0300

    Extract filenames to variables

--- pomodoro.sh
+++ pomodoro.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
 SOURCEDIR="$(cd "$(dirname "${0}")"; echo "$(pwd)")"
+pomodoro_icon="pomodoro-icon.png"
+pomodoro_work="pomodoro-work.png"  # Change this to .ico for a second option
+pomodoro_sound="to-the-point.ogg"
 
 start_pomodoro() {
     # 25 minute work
-    sleep 1500 && notify-send -u critical -i $SOURCEDIR/pomodoro-icon.png "It's time to take a break!"
-    paplay $SOURCEDIR/to-the-point.ogg
+    sleep 1500 && notify-send -u critical -i $SOURCEDIR/$pomodoro_icon "It's time to take a break!"
+    paplay $SOURCEDIR/$pomodoro_sound
 }
 
 short_break() {
     # a short, 5 minute break
-    sleep 300 && notify-send -u critical -i $SOURCEDIR/pomodoro-work.png "Back to work!"
-    paplay $SOURCEDIR/to-the-point.ogg
+    sleep 300 && notify-send -u critical -i $SOURCEDIR/$pomodoro_work "Back to work!"
+    paplay $SOURCEDIR/$pomodoro_sound
 }
 
 long_break() {
     # a short, 15 minute break
-    sleep 900 && notify-send -u critical -i $SOURCEDIR/pomodoro-work.png "Back to work!"
-    paplay $SOURCEDIR/to-the-point.ogg
+    sleep 900 && notify-send -u critical -i $SOURCEDIR/$pomodoro_work "Back to work!"
+    paplay $SOURCEDIR/$pomodoro_sound
 }
 
 # First pomodoro
