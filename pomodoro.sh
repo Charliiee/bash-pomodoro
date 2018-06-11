3f6b1939c9366433c75edabf926c3c0a92c9f10f
Carlos <carlos.braga@admcsc.com.br>
2018-06-11 15:24:56 -0300

    Add function to notify about short and long breaks

--- pomodoro.sh
+++ pomodoro.sh
@@ -7,4 +7,21 @@ start_pomodoro() {
     sleep 1500 && notify-send -u critical -i $SOURCEDIR/pomodoro-icon.png "It's time to take a break!"
 }
 
-start_pomodoro;
+short_break() {
+    # a short, 5 minute break
+    sleep 300 && notify-send -u critical -i $SOURCEDIR/pomodoro-work.png "Back to work!"
+}
+
+long_break() {
+    # a short, 15 minute break
+    sleep 900 && notify-send -u critical -i $SOURCEDIR/pomodoro-work.png "Back to work!"
+}
+
+# First pomodoro
+start_pomodoro; short_break;
+# Second pomodoro
+start_pomodoro; short_break;
+# Third pomodoro
+start_pomodoro; short_break;
+# Fourth pomodoro, take a longer break
+start_pomodoro; long_break;
