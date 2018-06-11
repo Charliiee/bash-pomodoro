ff9594e42b820499e67b6fe37ff110fe4f69b7d9
Carlos <carlos.braga@admcsc.com.br>
2018-06-11 15:22:44 -0300

    Add icon to notification

--- pomodoro.sh
+++ pomodoro.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-sleep 1500 && notify-send "Take a break!"
+SOURCEDIR="$(cd "$(dirname "${0}")"; echo "$(pwd)")"
+
+start_pomodoro() {
+    # 25 minute work
+    sleep 1500 && notify-send -u critical -i $SOURCEDIR/pomodoro-icon.png "It's time to take a break!"
+}
+
+start_pomodoro;
